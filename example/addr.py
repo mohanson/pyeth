ff58dbea81d6f83a96954643f0d4a71c0c7dc887
mohanson <mohanson@outlook.com>
2024-04-07 14:34:22 +0800

    Rewrite example/addr.py

--- example/addr.py
+++ example/addr.py
@@ -4,14 +4,10 @@ import eth
 # Calculate the address from a private key.
 
 parser = argparse.ArgumentParser()
-parser.add_argument('prikey', type=str, help='private key')
+parser.add_argument('--prikey', type=str, help='private key')
 args = parser.parse_args()
 
-base = 10
-if args.prikey.startswith('0x'):
-    base = 16
-
-prikey = eth.core.PriKey(int(args.prikey, base))
+prikey = eth.core.PriKey(int(args.prikey, 0))
 pubkey = prikey.pubkey()
 addr = pubkey.addr()
 print(addr)
