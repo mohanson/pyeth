eb9d0774d8823d9b5f1c43fe11904bb2c33c6cd6
mohanson <mohanson@outlook.com>
2024-04-09 14:24:08 +0800

    Implement transfer all in wallet

--- eth/wallet.py
+++ eth/wallet.py
@@ -25,15 +25,22 @@ class Wallet:
     def balance(self):
         return int(eth.rpc.eth_get_balance(f'0x{self.addr.hex()}', 'latest'), 0)
 
+    def nonce(self):
+        return int(eth.rpc.eth_get_transaction_count(f'0x{self.addr.hex()}', 'pending'), 0)
+
     def transfer(self, addr: bytearray, value: int):
-        tx = eth.core.TxLegacy(
-            int(eth.rpc.eth_get_transaction_count(f'0x{self.addr.hex()}', 'pending'), 0),
-            int(eth.rpc.eth_gas_price(), 0),
-            21000,
-            addr,
-            value,
-            bytearray(),
-        )
+        gas_price = int(eth.rpc.eth_gas_price(), 0)
+        gas = 21000
+        tx = eth.core.TxLegacy(self.nonce(), gas_price, gas, addr, value, bytearray())
+        tx.sign(self.prikey)
+        hash = eth.rpc.eth_send_raw_transaction(f'0x{tx.rlp().hex()}')
+        assert tx.hash() == bytearray.fromhex(hash[2:])
+        return tx.hash()
+
+    def transfer_all(self, addr: bytearray):
+        gas_price = int(eth.rpc.eth_gas_price(), 0)
+        gas = 21000
+        tx = eth.core.TxLegacy(self.nonce(), gas_price, gas, addr, self.balance() - gas * gas_price, bytearray())
         tx.sign(self.prikey)
         hash = eth.rpc.eth_send_raw_transaction(f'0x{tx.rlp().hex()}')
         assert tx.hash() == bytearray.fromhex(hash[2:])
--- test/test_wallet.py
+++ test/test_wallet.py
@@ -14,3 +14,13 @@ def test_transfer():
     eth.rpc.eth_wait(f'0x{hash.hex()}')
     b = hole.balance()
     assert b == a + 1 * eth.denomination.ether
+
+
+def test_transfer_all():
+    user = eth.wallet.Wallet(1)
+    hole = eth.wallet.Wallet(2)
+    hash = user.transfer(hole.addr, 1 * eth.denomination.ether)
+    eth.rpc.eth_wait(f'0x{hash.hex()}')
+    hash = hole.transfer_all(user.addr)
+    eth.rpc.eth_wait(f'0x{hash.hex()}')
+    assert hole.balance() == 0
