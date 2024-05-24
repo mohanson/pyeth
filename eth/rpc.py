a1056908509ee852a041492c71fc27eef7f2a0a0
mohanson <mohanson@outlook.com>
2024-05-24 12:21:30 +0800

    Add all rpc interfaces

--- eth/rpc.py
+++ eth/rpc.py
@@ -20,6 +20,10 @@ def call(method: str, params: typing.List) -> typing.Any:
     return r['result']
 
 
+def eth_accounts() -> typing.List[str]:
+    return call('eth_accounts', [])
+
+
 def eth_block_number() -> str:
     return call('eth_blockNumber', [])
 
@@ -30,6 +34,10 @@ def eth_call(body: typing.Dict, block_number: str) -> str:
 
 def eth_chain_id() -> str:
     return call('eth_chainId', [])
+
+
+def eth_coinbase() -> str:
+    return call('eth_coinbase', [])
 
 
 def eth_estimate_gas(body: typing.Dict, block_number: str) -> str:
@@ -64,6 +72,30 @@ def eth_get_code(addr: str, block_number: str) -> str:
     return call('eth_getCode', [addr, block_number])
 
 
+def eth_get_filter_changes(id: str) -> typing.List[typing.Dict]:
+    return call('eth_getFilterChanges', [id])
+
+
+def eth_get_filter_logs(id: str) -> typing.List[typing.Dict]:
+    return call('eth_getFilterLogs', [id])
+
+
+def eth_get_logs(option: typing.Dict) -> typing.List[typing.Dict]:
+    return call('eth_getLogs', [option])
+
+
+def eth_get_storage_at(addr: str, position: str, block_number: str) -> str:
+    return call('eth_getStorageAt', [addr, position, block_number])
+
+
+def eth_get_transaction_by_block_hash_and_index(hash: str, index: str):
+    return call('eth_getTransactionByBlockHashAndIndex', [hash, index])
+
+
+def eth_get_transaction_by_block_number_and_index(block_number: str, index: str):
+    return call('eth_getTransactionByBlockNumberAndIndex', [block_number, index])
+
+
 def eth_get_transaction_by_hash(hash: str) -> typing.Dict:
     return call('eth_getTransactionByHash', [hash])
 
@@ -76,12 +108,84 @@ def eth_get_transaction_receipt(hash: str) -> typing.Dict:
     return call('eth_getTransactionReceipt', [hash])
 
 
+def eth_get_uncle_by_block_hash_and_index(hash: str, index: str):
+    return call('eth_getUncleByBlockHashAndIndex', [hash, index])
+
+
+def eth_get_uncle_by_block_number_and_index(block_number: str, index: str):
+    return call('eth_getUncleByBlockNumberAndIndex', [block_number, index])
+
+
+def eth_get_uncle_count_by_block_hash(hash: str) -> str:
+    return call('eth_getUncleCountByBlockHash', [hash])
+
+
+def eth_get_uncle_count_by_block_number(block_number: str) -> str:
+    return call('eth_getUncleCountByBlockNumber', [block_number])
+
+
+def eth_hashrate() -> str:
+    return call('eth_hashrate', [])
+
+
 def eth_max_priority_fee_per_gas() -> str:
     return call('eth_maxPriorityFeePerGas', [])
 
 
+def eth_mining() -> bool:
+    return call('eth_mining', [])
+
+
+def eth_new_block_filter() -> str:
+    return call('eth_newBlockFilter', [])
+
+
+def eth_new_filter(option: typing.Dict) -> str:
+    return call('eth_newFilter', [option])
+
+
+def eth_new_pending_transaction_filter() -> str:
+    return call('eth_newPendingTransactionFilter', [])
+
+
+def eth_protocol_version() -> str:
+    return call('eth_protocolVersion', [])
+
+
 def eth_send_raw_transaction(tx: typing.Dict) -> str:
     return call('eth_sendRawTransaction', [tx])
+
+
+def eth_send_transaction(tx: typing.Dict) -> str:
+    return call('eth_sendTransaction', [tx])
+
+
+def eth_sign(addr: str, message: str) -> str:
+    return call('eth_sign', [addr, message])
+
+
+def eth_sign_transaction(tx: typing.Dict) -> str:
+    return call('eth_signTransaction', [tx])
+
+
+def eth_syncing() -> typing.Dict:
+    return call('eth_syncing', [])
+
+
+def eth_uninstall_filter(id: str):
+    return call('eth_uninstallFilter', [id])
+
+
+def net_version() -> str:
+    return call('net_version', [])
+
+
+def net_listening() -> bool:
+    return call('net_listening', [])
+
+
+def net_peer_count() -> str:
+    return call('net_peerCount', [])
 
 
 def wait(hash: str):
@@ -98,3 +202,11 @@ def wait(hash: str):
         if int(r['status'], 0) != 1:
             raise Exception(r['status'])
         break
+
+
+def web3_client_version() -> str:
+    return call('web3_clientVersion', [])
+
+
+def web3_sha3(data: str) -> str:
+    return call('web3_sha3', [data])
